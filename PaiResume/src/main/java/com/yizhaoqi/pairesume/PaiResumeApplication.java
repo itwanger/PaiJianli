b1119e4d9896875a3f95c4029d78c7a76662e97b
Lanyilei-art <84671561+Lanyilei-art@users.noreply.github.com>
2025-09-14 13:53:04 +0800

    create sprintboot project

---
+++ PaiResume/src/main/java/com/yizhaoqi/pairesume/PaiResumeApplication.java
@@ -0,0 +1,13 @@
+package com.yizhaoqi.pairesume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiResumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaiResumeApplication.class, args);
+    }
+
+}
---
+++ PaiResume/src/test/java/com/yizhaoqi/pairesume/PaiResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yizhaoqi.pairesume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaiResumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
